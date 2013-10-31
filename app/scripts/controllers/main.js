75eadb68226f7ede705f89ee59fae06ed3b61b7f
Andrey Pushkarev <fealaer@gmail.com>
2013-10-31 14:11:07 +0400

    ExpressJS added and configured to work with AngularJS

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,8 @@
 angular.module('StartingPointJSApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
+      'NodeJS',
+      'Express',
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
