bb78adc402274ee9ec7bbab7c32db2c955511b8e
Andrey Pushkarev <fealaer@gmail.com>
2013-11-06 23:49:25 +0400

    Nice error message for Mongoose ValidationError and MongoError

--- helpers/api/v0/error.js
+++ helpers/api/v0/error.js
@@ -4,23 +4,19 @@ var ENV = process.env.NODE_ENV;
 
 function ApiError(err, code, message) {
   if (err) {
-    log.error(util.format("%j", err));
+    log.error(err.stack);
     if (err.name == 'ValidationError') {
-      log.error(util.format("%j", err));
-//      for (field in err.errors) {
-//        switch (err.errors[field].type) {
-//          case 'exists':
-//
-//            break;
-//          case 'invalid':
-//          ...
-//            break;
-//          ...
-//        }
-//      }
-    } else if (err.name == "MongoError" && err.code == 11000) {
       this.message = err.message;
-      this.code = 404;
+      this.code = 400;
+      this.errors = [];
+      for (var errName in err.errors) {
+        this.errors.push({
+          path: err.errors[errName].path,
+          message: err.errors[errName].message});
+      }
+    } else if (err.name == "MongoError" && err.code == 11000) {
+      this.message = extractFieldNameAndValue(err.message);
+      this.code = 400;
     } else {
       this.message = 'Internal server error';
       this.code = 500;
@@ -29,10 +25,25 @@ function ApiError(err, code, message) {
     this.message = message;
     this.code = code;
   }
-  this.moreInfo = util.format('http://localhost:3000/api/docs/errors/%d', this.code);
+  this.moreInfo = util.format('http://localhost:3000/api/v0/docs/errors/%d', this.code);
 }
 
 
 ApiError.prototype.name = "ApiError";
 
 module.exports = ApiError;
+
+function extractFieldNameAndValue(msg) {
+  var start = msg.indexOf('.$') + 2;
+  var end = msg.indexOf('_', start);
+  var dupKey = msg.indexOf("dup key:", end);
+  var startValue = msg.indexOf('"', dupKey) + 1;
+  var endValue = msg.indexOf('"', startValue);
+
+  var field = msg.substr(start, end - start);
+  var value = msg.substr(startValue, endValue - startValue);
+
+  return util.format("Not unique value '%s' for field '%s'", value, field);
+
+  //"username must be unique"
+}
