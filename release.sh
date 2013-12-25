8dc590870853aa5d7b455919099f92eead7e329d
Andrey Pushkarev <fealaer@gmail.com>
2013-12-25 13:06:46 +0400

    Added better project description with how to use information

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+git checkout release
+git pull origin master -qf
 grunt build
 git add .
-git commit -m 'Build'
+git commit -m 'New Build'
 git push heroku release:master
+git checkout -
